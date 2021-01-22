34f238b86b294ecfccfd4b2e0407b502a80f0112
Bo-Eamonn <bo.eamonn@gmail.com>
2021-01-22 17:54:36 +0100

    Bo | added medication crud

---
+++ src/Form/MedicationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Medication;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MedicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('effect')
+            ->add('price')
+            ->add('insured')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Medication::class,
+        ]);
+    }
+}
